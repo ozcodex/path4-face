3ac7216d20d540e8ec9a967ea1535edff8095b5c
Oscar Bernal <oscarbernal93@gmail.com>
2019-05-21 19:27:57 +0200

    added the logo as a React component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import './App.css';
+import './Logo.css';
 
 class App extends Component {
   render() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 
 import React, { Component } from 'react';
+import { Container,Col,Row } from 'react-bootstrap';
 import Background from './Background.jsx';
 import Banner from './Banner.jsx'
-
+import Logo from './Logo.jsx'
 
 class Home extends Component {
   render() {
     return (
         <div>
           <Background />
-          <Banner />
+          <Container>
+            <Logo />
+            <Banner />
+          </Container>
         </div>
     );
   }
