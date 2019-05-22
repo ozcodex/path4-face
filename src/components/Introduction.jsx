1c59300ae3d7fd897bcb92c15d021cd4b62e48f6
luzhang1311 <zlsx@mail.ustc.edu.cn>
2019-05-22 21:08:34 +0200

    teamname

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -11,7 +11,7 @@ class Introduction extends React.Component {
     alignItems: 'center',
   }}  height="200" width="200" src={crossing} alt="crossing"/>
          {/* <p style={{color: 'red'}}>This is a paragraph.</p> */}
-         <p className="text-center" style={{color: 'white',fontWeight: 'bold',fontSize:'40px'}}>Path 4: There are 4 directions when we stand at a crossing,so we could use path4 to choose the direction. Also, we have 4 winds in some culture</p>
+         <p className="text-center" style={{color: 'white',fontWeight: 'bold',fontSize:'40px'}}>Path 4: There are 4 directions when we stand at a crossing,so we could use path4 to choose a good direction.<br></br> Also, we want to provide you with the best public transportation app experience by suggesting the best way(Path) for(4) you.</p>
         </div>
     );
 
