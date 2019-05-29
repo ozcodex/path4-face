9b143caf352bd49bab9cfe36449c9d0dfbddcede
Oscar Bernal <oscarbernal93@gmail.com>
2019-05-29 09:21:23 +0200

    Added connection to the database in the backend

--- api/server.js
+++ api/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const cors = require('cors');
+const db = require('./db.js')
 const app = express();
+
+const port = 4242;
 
 app.use(cors());
 
-const port = 4242;
 app.get('/hello', function (req, res) {
   res.json({ message: 'Hello Nerds' })
+});
+
+app.get('/random', function (req, res) {
+  db.collection('messages').get()
+    .then(snapshot => {
+      let rand_pos = Math.floor(Math.random() * snapshot.size);
+      let word = snapshot.docs[rand_pos].data();
+      // this object has this structure:
+      // { text: string }
+      res.json(word);
+    })
+      .catch(err => {
+      console.log('Error getting collection messages in /random', err);
+    });
 });
 
 app.listen(port, () => {
